a664075cf1ed80ea0cad66d76350ab936aab7513
Patryk <Patryk@Shadow>
2013-02-24 00:12:36 +0100

    Pierwszy commit

---
+++ Prb/src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		float f =1/3;
+		int i= 1/3;
+		float f2 = (float)1.01;
+		double d=999d;
+		
+		System.out.println(0x10 *1l*300.0);
+		
+	}
+
+}
